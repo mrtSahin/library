4b1ade4262db665561b0c7b2cd4c1e0cdd907133
mrtSahin <mrtsvs07@gmail.com>
2023-03-18 09:42:01 +0300

    first commit

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent,Button } from 'library'
+import 'library/dist/index.css'
+
+const App = () => {
+  return <Button text={"NPM kutuphanesi yaziyorum"}/>
+}
+
+export default App

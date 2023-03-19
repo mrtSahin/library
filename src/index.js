06765033804bdd5ee8c4be8bcfb5699bd96b8704
mrtSahin <mrtsvs07@gmail.com>
2023-03-20 00:26:51 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
-
 export const Button =(props)=>{
   return (<button {...props}>{props.text}</button>) // kod içerisinde girilen propertileri prop şeklinde alır ve böylece hepsini tek tek yazmak yerine {prop} şeklinde kısaca kullanırız.
 }
